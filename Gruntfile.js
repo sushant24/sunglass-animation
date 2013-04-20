0d0c756ef499efba60d77271b1ca102ee08094c9
Sushant Bhatia <bhatia.sushant24@gmail.com>
2013-04-19 23:19:37 -0700

    Math is working

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,6 +2,13 @@ module.exports = function(grunt) {
     grunt.initConfig({
         pkg: grunt.file.readJSON('package.json'),
 
+        watch : {
+            compass  :{
+                files : ["sass/*"],
+                tasks : ["compass:project"]
+            }
+        },
+
         compass: {
             project : {
                 options: {
@@ -17,8 +24,10 @@ module.exports = function(grunt) {
 
     grunt.loadNpmTasks('grunt-contrib-sass');
     grunt.loadNpmTasks('grunt-contrib-compass');
+    grunt.loadNpmTasks('grunt-contrib-watch');
 
     grunt.registerTask('build', ['compass']);
+    grunt.registerTask('start', ['build','watch']);
 
     grunt.registerTask('default', ['build']);
 
